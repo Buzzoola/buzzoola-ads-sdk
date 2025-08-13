9acc59b54d29bcfe20c352582efd7abccd9813b8
Коротаева Елена Сергеевна <ekorotaeva@mts.ru>
2025-08-13 16:14:00 +0500

    version 3.3.0

---
+++ BuzzoolaAdsSDK/3.3.0/BuzzoolaAdsSDK.podspec
@@ -0,0 +1,46 @@
+#
+#  Be sure to run `pod spec lint BuzzoolaAdsSDK.podspec' to ensure this is a
+#  valid spec and to remove all comments including this before submitting the spec.
+#
+#  To learn more about Podspec attributes see https://guides.cocoapods.org/syntax/podspec.html
+#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
+#
+
+Pod::Spec.new do |s|
+
+  s.name             = 'BuzzoolaAdsSDK'
+  s.version          = '3.3.0'
+  s.summary          = 'Mobile SDK for Ads'
+  s.swift_version    = '5.0'
+
+  s.description      = <<-DESC
+TODO: Add long description of the pod here.
+                       DESC
+
+  s.homepage         = 'https://gitlab.services.mts.ru/mts-ads/sdk/mts-ads-sdk-ios'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'eskorotaeva' => 'ekorotaeva@mts.ru' }
+  s.source           = { :http => 'https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDK_3.3.0.zip', :type => 'zip' }
+
+  s.ios.deployment_target = '13.0'
+  s.static_framework = true
+  s.requires_arc = true
+
+  s.subspec 'BuzzoolaAdsSDK' do |ss|
+    ss.vendored_frameworks = 'BuzzoolaAdsSDK/BuzzoolaAdsSDK.xcframework'
+
+    ss.dependency 'SDWebImage', '~> 5.20'
+    ss.dependency 'DeviceKit', '~> 5.5'
+  end
+
+  s.subspec 'BuzzoolaAdsSDKAnalytics' do |ss|
+    ss.vendored_frameworks = 'BuzzoolaAdsSDK/BuzzoolaAdsSDKAnalytics.xcframework'
+    ss.dependency 'MTAnalytics', '~> 5.0'
+  end
+
+  s.subspec 'BuzzoolaAdsSDKMyTarget' do |ss|
+    ss.vendored_frameworks = 'BuzzoolaAdsSDK/BuzzoolaAdsSDKMyTarget.xcframework'
+    ss.dependency 'myTargetSDK', '~> 5.29'
+  end
+
+end
--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/MobileTeleSystems/mts-analytics-swiftpm-ios-sdk", exact: "5.1.4"),
+            url: "https://github.com/MobileTeleSystems/mts-analytics-static-swiftpm-ios-sdk", exact: "5.1.4"),
         .package(
             url: "https://github.com/devicekit/DeviceKit.git", exact: "5.5.0"),
         .package(
@@ -32,15 +32,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BuzzoolaAdsSDK",
-            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDK_3.2.0.xcframework.zip",
-            checksum: "e5d15ed486cf4fc2a4676bfae8df5aaa8357ad15a9b06feab068749ed1841636"),
+            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDK_3.3.0.xcframework.zip",
+            checksum: "df0c7a9a80d9430148758cdcccb2f035b43a002eae1f09b588ed62a94b780060"),
         .binaryTarget(
             name: "BuzzoolaAdsSDKMyTarget",
-            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDKMyTarget_3.2.0.xcframework.zip",
-            checksum: "a72b3e25b4d0b75e536c26cde50ded7ff769d8c26a10b8dbd2cbea85e1dcf0c9"),
+            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDKMyTarget_3.3.0.xcframework.zip",
+            checksum: "07ad735635128ec04cca1f6c9fe4b31631412cf86ea24f591fccefafb06eff2e"),
         .binaryTarget(
             name: "BuzzoolaAdsSDKAnalytics",
-            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDKAnalytics_3.2.0.xcframework.zip",
-            checksum: "106194d1aaf69d83995f589438aae281306b17b03f60c77ac97d0aa794700f65")
+            url: "https://ru-msk-1.store.cloud.mts.ru/monetization.download/repository/ios-sdk/BuzzoolaAdsSDKAnalytics_3.3.0.xcframework.zip",
+            checksum: "a1f3172161f6dc71126100f41710bf75b693bd0daf1abc39b69d6c69c6500e1c")
     ]
 )
